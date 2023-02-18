dc8f5e3fcda4cb1cf9da70a7ba1ecfebbc3c4833
alasdairkite93 <alasdairkite93@gmail.com>
2023-02-18 21:30:40 +0000

    Git Proj

--- sites.py
+++ sites.py
@@ -123,8 +123,8 @@ class Zoopla:
 
         print('search url: ', searchurl)
 
-        # file = open('/home/alasdairkite/flasksearch/static/urls.txt', 'w')
-        file = open('/static/urls.txt', 'w')
+        file = open('/home/alasdairkite/flasksearch/static/urls.txt', 'w')
+        # file = open('/static/urls.txt', 'w')
         prox = proxy.getProxy()
         print("Writing proxy to file: ", prox)
         file.write(searchurl)
@@ -328,8 +328,8 @@ class OnTheMarket:
         for i in range(1):
             print('i in num: ', i)
 
-            # file = open('/home/alasdairkite/flasksearch/static/urls.txt', 'w')
-            file = open('./static/urls.txt', 'w')
+            file = open('/home/alasdairkite/flasksearch/static/urls.txt', 'w')
+            # file = open('./static/urls.txt', 'w')
 
             prox = proxy.getProxy()
             file.write(url+',')
@@ -556,8 +556,8 @@ class Gumtree:
 
 
         proxy = Proxies()
-        file = open('static/urls.txt', 'w')
-        # file = open('/home/alasdairkite/flasksearch/static/urls.txt', 'w')
+        # file = open('static/urls.txt', 'w')
+        file = open('/home/alasdairkite/flasksearch/static/urls.txt', 'w')
 
         prox = proxy.getProxy()
         print("Writing proxy to file: ", prox)
